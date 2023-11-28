baaaeccf4e219adc5fd6cd136c6b5017ad4b0b63
Thilagavathi Amulraj <Thilagavathi.Amulraj90@gmail.com>
2023-11-28 12:23:09 -0600

    Commit movie-info-service

---
+++ src/test/java/com/myplayground/movieinfoservice/MovieResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myplayground.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
